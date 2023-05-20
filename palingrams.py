b7afbf0057e91dfaf5552b62d6303f0c7a45b3f6
Diana <102248347+dididiana07@users.noreply.github.com>
2023-05-20 16:04:41 +0200

    Add files via upload

---
+++ palingrams.py
@@ -0,0 +1,33 @@
+import json
+
+def load(json_file):
+    """Return the content of a JSON file."""
+    try:
+        with open(json_file, "r") as file_open:
+            data = json.load(file_open)
+    except json.decoder.JSONDecodeError:
+        return "Not a JSON file."
+    return data
+
+
+def palingram(word: str, dictionary_words: dict):
+    """Find if a word is a palingram"""
+    palingram_lst = []
+    reversed_word = word[::-1]
+    for w in dictionary_words:
+        if reversed_word in w:
+            palingram_lst.append(w)
+    return palingram_lst
+
+def main():
+    dictionary = load("2_finding_palingram_spells/words_dictionary.json")
+    word = input("Word: ")
+    results = palingram(word, dictionary)
+    print(f"Results: {len(results)}")
+    print("---------------")
+    print("\n".join(results))
+
+
+
+if __name__ == "__main__":
+    main()
